7a91fa30d4176e2424166fdc81f267a67a7f204e
unityjaeger <=>
2026-07-12 22:49:10 +0200

    add pages link

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: "https://unityjaeger.github.io",
+	base: "/reel/",
 	integrations: [
 		starlight({
 			title: 'Reel',
